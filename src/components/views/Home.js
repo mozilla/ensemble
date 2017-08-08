edec3ec72bb3bb38f1afa1dc9b9cfe24f146f764
John Karahalis <john.karahalis@gmail.com>
2017-08-08 18:01:59 -0400

    Fix link to GitHub project page

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 export default () => (
     <article>
-        <p>Ensemble is minimalist platform for publishing data. See the <Link to="https://github.com/openjck/ensemble">GitHub project page</Link> for more infomration.</p>
+        <p>Ensemble is minimalist platform for publishing data. See the <a href="https://github.com/openjck/ensemble">GitHub project page</a> for more infomration.</p>
         <section>
             <h2>Example dashboards</h2>
             <nav>
