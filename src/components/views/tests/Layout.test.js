a9e5fbb85e12a1f506f0642f29efa8d2a4b99a4b
John Karahalis <john.karahalis@gmail.com>
2017-08-15 14:15:20 -0400

    Remove obsolete test

--- src/components/views/tests/Layout.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Layout from '../Layout';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Layout />, div);
-});
