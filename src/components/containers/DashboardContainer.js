83a2f5cdcf684f22823a1b67d083c1025b9924e8
John Karahalis <john.karahalis@gmail.com>
2017-08-24 17:27:14 -0400

    Add a loading spinner

--- src/components/containers/DashboardContainer.js
+++ src/components/containers/DashboardContainer.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import Spinner from 'react-spinkit';
 import { connect } from 'react-refetch';
 
-import Loading from '../views/Loading';
 import Error from '../views/Error';
 import Dashboard from '../views/Dashboard';
+
+import './css/Spinner.css';
 
 
 const DashboardContainer = props => {
     const dataFetch = props.dataFetch;
 
     if (dataFetch.pending) {
-        return <Loading />;
+        return <Spinner name="circle" fadeIn="none" />;
     } else if (dataFetch.rejected) {
         return <Error message={dataFetch.reason.message} />;
     } else if (dataFetch.fulfilled) {
