4fa79dce67ca633422147c8d213a99ece894edb3
John Karahalis <john.karahalis@gmail.com>
2017-08-24 15:38:03 -0400

    Move "example data" warning to dashboard pages
    Ensemble is almost ready to start hosting real data, even if the
    dashboards for that data are pretty simple. So I'd like to start
    treating it like a real product. The data warning won't affect all of
    our datasets that Ensemble can host, only example datasets, so it should
    be shown there.

--- src/components/views/Header.js
+++ src/components/views/Header.js
@@ -4,6 +4,5 @@ import { Link } from 'react-router-dom';
 export default () => (
     <header id="main-header">
         <h1><Link to="/">Ensemble</Link></h1>
-        <p id="data-warning">This is an example. These dashboards do not display any real data.</p>
     </header>
 );
