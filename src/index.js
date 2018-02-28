6758eec2d97fdc6bce08cb3424bf847a9d4098f5
John Karahalis <john.karahalis@gmail.com>
2018-02-28 17:42:00 -0500

    Working, but acting weird

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 import Application from './components/views/Application';
 
 
-ReactDOM.render((
+// See the react-snap documentation
+// https://github.com/stereobooster/react-snap#basic-usage-with-create-react-app
+const root = document.getElementById('root');
+const renderFn = root.hasChildNodes() ? hydrate : render;
+
+renderFn((
     <BrowserRouter>
         <Application />
     </BrowserRouter>
-), document.getElementById('root'));
+), root);
 
 registerServiceWorker();
