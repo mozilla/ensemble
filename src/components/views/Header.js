7826c317f5007f80d23dad8a9907728fd216666f
John Karahalis <john.karahalis@gmail.com>
2018-03-02 19:03:54 -0500

    Add the new "Health" dashboard

--- src/components/views/Header.js
+++ src/components/views/Header.js
@@ -3,14 +3,21 @@ import { Link } from 'react-router-dom';
 
 import './css/Header.css';
 
+import dashboards from '../../config/dashboards.json';
+
 
 export default () => (
     <header id="main-header">
         <h1><Link to="/">Firefox Usage Report</Link></h1>
         <nav id="main-navigation">
             <ul>
-                <li><Link to="/dashboard/usage">Usage</Link></li>
-                <li><Link to="/dashboard/hardware">Hardware</Link></li>
+                {dashboards.map((dashboard, index) => {
+                    return (
+                        <li key={index}>
+                            <Link to={`/dashboard/${dashboard.path}`}>{dashboard.menuTitle}</Link>
+                        </li>
+                    );
+                })}
             </ul>
         </nav>
     </header>
