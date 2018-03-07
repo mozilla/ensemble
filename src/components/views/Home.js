b4ac6b924d7072a0e6a5c9a6a2352b712d951181
John Karahalis <john.karahalis@gmail.com>
2018-03-07 00:37:26 -0500

    Fix #22: Add real homepage text

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -4,58 +4,29 @@ import React from 'react';
 export default () => (
     <article>
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel
-            tellus ipsum. Phasellus sodales et neque ut lobortis. Morbi mollis
-            enim non ultrices tristique. Curabitur vel dolor vel dolor interdum
-            lobortis ac vitae magna. Nunc volutpat tellus ex, et gravida lorem
-            facilisis nec. Proin diam ligula, tincidunt id fermentum
-            pellentesque, placerat nec felis. Donec rhoncus ipsum aliquet orci
-            vestibulum, vel pulvinar neque elementum. Vestibulum dictum ligula
-            lorem, quis ultrices purus placerat ac. Sed mollis, ex et mollis
-            molestie, quam tortor eleifend erat, eget consequat ex eros id quam.
+            The Firefox Public Data Report is a weekly public report on the
+            health, usage, and hardware configuration of Firefox.
         </p>
         <p>
-            Duis faucibus fermentum placerat. Morbi sollicitudin maximus tortor,
-            id tincidunt neque. Ut iaculis lacus diam, eu convallis ante
-            vehicula non. In arcu mi, aliquet a tincidunt hendrerit, porta a
-            est. Suspendisse finibus fermentum nunc, vitae fermentum lorem
-            molestie id. Nullam a turpis semper, tempus eros eget, ornare justo.
-            Praesent non ex tellus.
+            The purpose of this report is twofold. One: empower developers,
+            journalists, and the overall public to better understand the state
+            of the web and the direction of trends in desktop web browsing. Two:
+            transparency. We believe in an open web. Data from the public should
+            go to the public, for the public.
         </p>
         <p>
-            Integer malesuada quam dictum elementum gravida. Nam bibendum magna
-            nec ante dictum, laoreet ullamcorper nulla semper. Nullam faucibus,
-            dui et bibendum rhoncus, mauris arcu pellentesque tellus, et
-            dignissim nibh magna nec risus. Nulla interdum quis neque ultrices
-            tristique. Nullam dictum purus at euismod lacinia. Nam congue
-            condimentum mauris, at finibus diam condimentum vitae. In sed massa
-            sem. Suspendisse odio libero, eleifend nec finibus eu, auctor eget
-            leo. Nulla venenatis massa ut ante viverra suscipit. Vivamus vel
-            convallis ipsum, eu condimentum tellus. Praesent convallis accumsan
-            ligula, ut euismod quam mollis quis. Integer sit amet tristique
-            odio, vel sodales risus. Aliquam erat volutpat. Nunc sem ipsum,
-            ullamcorper ut vehicula nec, efficitur nec odio. Cras sit amet est
-            gravida, consequat urna in, varius augue.
+            The report is split into 3 sections: Health of Firefox, Web Usage,
+            and Hardware Across the Web. Health of Firefox includes metrics for
+            the the overall Firefox user population. Web Usage describes ways in
+            which Firefox is being used. Hardware Across the Web details
+            hardware specs and configurations for the machines running Firefox. 
         </p>
         <p>
-            In nulla justo, molestie quis venenatis vitae, lobortis vitae diam.
-            Curabitur hendrerit egestas hendrerit. Pellentesque malesuada
-            aliquam risus vel placerat. Nunc eget pretium nulla. Aliquam feugiat
-            scelerisque leo. Integer nec felis in ex lobortis facilisis. Integer
-            vel turpis massa. Nunc nec iaculis ante. Pellentesque dolor leo,
-            tristique a porttitor eget, facilisis id neque. Vestibulum rutrum,
-            dolor non venenatis luctus, dui turpis pellentesque risus, non
-            malesuada urna nisi ut tortor. Etiam pulvinar felis at purus
-            tincidunt, vel rhoncus est iaculis.
-        </p>
-        <p>
-            Cras convallis ultricies felis ut pulvinar. Quisque eget posuere
-            urna. Mauris tempus mollis orci, ac pharetra augue aliquet porta.
-            Proin porta quam at nunc tincidunt consequat et ac augue. Praesent
-            viverra ultrices dolor nec fringilla. Donec et semper risus. Cras ut
-            eros mi. In hac habitasse platea dictumst. Aliquam in mauris arcu.
-            Sed vel purus lacus. Duis congue et libero nec pulvinar.
-            Pellentesque posuere sed dui id ornare.
+            All data is from a representative 1% sample from our Release channel
+            and the report runs once a week. Each datapoint covers a week’s
+            worth of data (unless stated otherwise). All data is anonymized and
+            aggregated to ensure user privacy. Links to definitions for specific
+            metrics and how they were generated can be found below. 
         </p>
     </article>
 );
