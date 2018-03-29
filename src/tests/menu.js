b4540c8ecfdd186f35c3bbb7425cf3c57757e396
John Karahalis <john.karahalis@gmail.com>
2018-03-29 16:46:44 -0400

    Test that menu items are in correct order

---
+++ src/tests/menu.js
@@ -0,0 +1,11 @@
+module.exports = {
+    before: browser => {
+        browser.url(browser.launchUrl);
+    },
+
+    'Menu items appear in correct order': browser => {
+        browser.expect.element('#main-navigation li:nth-child(1) a').text.to.equal('User Activity');
+        browser.expect.element('#main-navigation li:nth-child(2) a').text.to.equal('Usage Behavior');
+        browser.expect.element('#main-navigation li:nth-child(3) a').text.to.equal('Hardware');
+    },
+};
