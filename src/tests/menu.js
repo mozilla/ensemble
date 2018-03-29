3d8f39bea075ef55075207d5ff213ab9f579b14c
John Karahalis <john.karahalis@gmail.com>
2018-03-29 17:08:59 -0400

    Test that menu links work

--- src/tests/menu.js
+++ src/tests/menu.js
@@ -8,4 +8,15 @@ module.exports = {
         browser.expect.element('#main-navigation li:nth-child(2) a').text.to.equal('Usage Behavior');
         browser.expect.element('#main-navigation li:nth-child(3) a').text.to.equal('Hardware');
     },
+
+    'Menu links work': browser => {
+        function linkWorks(result) {
+            browser.url(result.value);
+            browser.expect.element('#not-found').to.not.be.present;
+        }
+
+        browser.getAttribute('#main-navigation li:nth-child(1) a', 'href', linkWorks);
+        browser.getAttribute('#main-navigation li:nth-child(2) a', 'href', linkWorks);
+        browser.getAttribute('#main-navigation li:nth-child(3) a', 'href', linkWorks);
+    }
 };
