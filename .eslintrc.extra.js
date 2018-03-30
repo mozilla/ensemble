ea87581ddf0ae5f474ec5b5d4998f120236e3f14
John Karahalis <john.karahalis@gmail.com>
2018-03-30 19:24:27 -0400

    Issue #67: Add extra eslint testing

---
+++ .eslintrc.extra.js
@@ -0,0 +1,37 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:jsx-a11y/recommended',
+    ],
+    parser: "babel-eslint",
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    plugins: [
+        'json',
+        'jsx-a11y',
+        'react',
+    ],
+    root: true,
+    rules: {
+        'jsx-a11y/label-has-for': ['error', {required: {every: ['id']}}],
+        'jsx-a11y/no-onchange': 'warn',
+        'react/display-name': 'off',
+        'react/prop-types': 'off',
+
+        'eqeqeq': 'error',
+        'no-console': 'warn',
+        'no-var': 'error',
+        'prefer-const': 'error',
+    },
+};
