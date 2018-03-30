c578e08aa53de68ed9b957ae31d1ed68c0a1c5f0
John Karahalis <john.karahalis@gmail.com>
2018-03-30 16:41:35 -0400

    Fix #25: Remove special error handling in DC
    The production site now shows fake data, so this warning will never be
    seen.

--- src/components/containers/DashboardContainer.js
+++ src/components/containers/DashboardContainer.js
@@ -26,32 +26,6 @@ class DashboardContainer extends React.Component {
         if (dataFetch.pending) {
             return <Spinner name="circle" fadeIn="none" />;
         } else if (dataFetch.rejected) {
-
-            // TODO: Remove this all when Ensemble launches
-            if (dataFetch.reason instanceof TypeError) {
-                if (this.props.match.params.key === 'usage-behavior') {
-                    const privateUsageBehaviorURL = 'https://metrics.mozilla.com/protected/usage-report-demo/dashboard/usage-behavior';
-                    return (
-                        <p>
-                            The Usage Behavior dashboard is not currently public and
-                            cannot be shown here. Visit the <a
-                            href={privateUsageBehaviorURL}>private demo</a> to see it.
-                            You will need to log in with your LDAP credentials.
-                        </p>
-                    );
-                } else if (this.props.match.params.key === 'user-activity') {
-                    const privateUserActivityURL = 'https://metrics.mozilla.com/protected/usage-report-demo/dashboard/user-activity';
-                    return (
-                        <p>
-                            The User Activity dashboard is not currently public and
-                            cannot be shown here. Visit the <a
-                            href={privateUserActivityURL}>private demo</a> to see it.
-                            You will need to log in with your LDAP credentials.
-                        </p>
-                    );
-                }
-            }
-
             return (
                 <Error
                     id="dashboard-fetch-error"
