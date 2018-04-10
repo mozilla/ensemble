6ad102fb8dc1dfad610608a0781cebb2d3a2e392
John Karahalis <john.karahalis@gmail.com>
2018-04-10 16:40:24 -0400

    Fix #101: Finalize homepage copy
    This includes only one significant change: adjusting the punctuation in
    the explanation of transparency. The rest of the changes are just
    formatting text to a width of 80 characters.

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -7,23 +7,24 @@ import './css/Home.css';
 export default () => (
     <article id="introduction">
         <p>
-            The Firefox Public Data Report is a weekly public report on the activity,
-            behavior, and hardware configuration of Firefox Desktop users.
+            The Firefox Public Data Report is a weekly public report on the
+            activity, behavior, and hardware configuration of Firefox Desktop
+            users.
         </p>
         <h2>The purpose of this report is twofold:</h2>
         <dl className="explanations split">
             <div>
                 <dt>Empowerment</dt>
                 <dd>
-                    We want to empower developers, journalists, and the overall public
-                    to better understand the state of the web and the direction of trends
-                    in desktop web browsing.
+                    We want to empower developers, journalists, and the overall
+                    public to better understand the state of the web and the
+                    direction of trends in desktop web browsing.
                 </dd>
             </div>
             <div>
                 <dt>Transparency</dt>
                 <dd>
-                    At Mozilla, we like to say that we are "Open by Design". We
+                    At Mozilla, we like to say that we are "Open by Design." We
                     believe in an open web, so data and insights from the public
                     should be made public, so the public can benefit.
                 </dd>
@@ -55,7 +56,8 @@ export default () => (
             and <Link to="https://www.mozilla.org/privacy/firefox/">data collection process</Link>.
         </p>
         <p>
-            You can learn more about this report by reading our announcement on the Mozilla blog.
+            You can learn more about this report by reading our announcement on
+            the Mozilla blog.
         </p>
         <div className="next-link">
             <Link to="/dashboard/user-activity">Proceed to User Activity</Link>
