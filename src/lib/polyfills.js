8936b6a1e90d3003181f901b304c1093d8e55238
John Karahalis <john.karahalis@gmail.com>
2018-04-20 05:31:39 -0400

    Issue #124: Attempt to fix IE 11 crashes

---
+++ src/lib/polyfills.js
@@ -0,0 +1,10 @@
+// Internet Explorer 11 has no support for these...
+import 'core-js/fn/array/find';
+import 'core-js/fn/array/includes';
+import 'core-js/es6/object/assign';
+import 'core-js/es6/promise';
+import 'whatwg-fetch';
+
+// Internet Explorer 11 has incomplete support for these...
+import 'core-js/es6/map';
+import 'core-js/es6/set';
