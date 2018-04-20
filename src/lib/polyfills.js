af120b844d28e6cd62cb23efc554a97e60443ad3
John Karahalis <john.karahalis@gmail.com>
2018-04-20 05:38:36 -0400

    Issue #124: Import polyfills, fix object.assign

--- src/lib/polyfills.js
+++ src/lib/polyfills.js
@@ -1,7 +1,7 @@
 // Internet Explorer 11 has no support for these...
 import 'core-js/fn/array/find';
 import 'core-js/fn/array/includes';
-import 'core-js/es6/object/assign';
+import 'core-js/fn/object/assign';
 import 'core-js/es6/promise';
 import 'whatwg-fetch';
 
