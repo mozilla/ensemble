c327aada9d4057784316ddc18d070728e2ad1b95
Davor Spasovski <davor@spasovski.net>
2018-05-10 13:16:37 -0500

    review related cleanup

--- src/components/views/Footer.js
+++ src/components/views/Footer.js
@@ -1,64 +1,87 @@
 import React from 'react';
 
 
-export default () => {
-    return (
-        <footer>
-            <div className="footer-primary">
-                <a className="footer-logo" href="https://www.mozilla.org/" target="_blank" rel="noopener noreferrer">Mozilla</a>
-                <ul>
-                    <li><h3>Mozilla</h3></li>
-                    <li>
-                        <a href="https://www.mozilla.org/about/" target="_blank" rel="noopener noreferrer">About</a>
-                    </li>
-                    <li>
-                        <a href="https://www.mozilla.org/contact/" target="_blank" rel="noopener noreferrer">Contact Us</a>
-                    </li>
-                    <li><a href="https://donate.mozilla.org/" target="_blank" rel="noopener noreferrer">Donate</a></li>
-                    <li>
-                        <a href="https://wiki.mozilla.org/Webdev/GetInvolved/mozilla.org" target="_blank" rel="noopener noreferrer">
-                            Contribute to this site
-                        </a>
-                    </li>
-                    <li className="footer-social">
-                        <a href="https://twitter.com/mozilla" target="_blank" rel="noopener noreferrer">Twitter <span>(@mozilla)</span></a>
-                        <a href="https://www.facebook.com/mozilla" target="_blank" rel="noopener noreferrer">Facebook <span>(Mozilla)</span></a>
-                        <a href="https://www.instagram.com/mozillagram/" target="_blank" rel="noopener noreferrer">
-                            Instagram <span>(@mozillagram)</span>
-                        </a>
-                    </li>
-                </ul>
-                <ul>
-                    <li><h3>Firefox</h3></li>
-                    <li><a href="https://www.mozilla.org/firefox/new/" target="_blank" rel="noopener noreferrer">Download Firefox Web browser</a></li>
-                    <li>
-                        <a href="https://www.mozilla.org/firefox/desktop/" target="_blank" rel="noopener noreferrer">
-                            Desktop Browser for Mac, Window, Linux
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.mozilla.org/firefox/android/" target="_blank" rel="noopener noreferrer">
-                            Mobile Browser for Android
-                        </a>
-                    </li>
-                    <li><a href="https://www.mozilla.org/firefox/ios/" target="_blank" rel="noopener noreferrer">Mobile Browser for iOS</a></li>
-                    <li className="footer-social">
-                        <a href="https://twitter.com/firefox" target="_blank" rel="noopener noreferrer">Twitter <span>(@firefox)</span></a>
-                        <a href="https://www.facebook.com/Firefox" target="_blank" rel="noopener noreferrer">Facebook <span>(Firefox)</span></a>
-                        <a href="https://www.youtube.com/firefoxchannel" target="_blank" rel="noopener noreferrer">YouTube <span>(firefoxchannel)</span></a>
-                    </li>
-                </ul>
-            </div>
-            <div className="footer-secondary">
-                <ul>
-                    <li><a href="https://www.mozilla.org/privacy/" target="_blank" rel="noopener noreferrer">Privacy</a></li>
-                    <li><a href="https://www.mozilla.org/privacy/websites/#cookies" target="_blank" rel="noopener noreferrer">Cookies</a></li>
-                    <li><a href="https://www.mozilla.org/about/legal/" target="_blank" rel="noopener noreferrer">Legal</a></li>
-                    <li><a href="https://www.mozilla.org/about/legal/fraud-report/" target="_blank" rel="noopener noreferrer">Report Trademark Abuse</a></li>
-                </ul>
-                <p>Portions of this content are ©1998–2018 by individual mozilla.org contributors. Content available under a {' '}
-                <a href="https://www.mozilla.org/foundation/licensing/website-content/" target="_blank" rel="noopener noreferrer">Creative Commons license</a>.</p>
-            </div>
-        </footer>
-    );
-};
+export default class extends React.Component {
+    componentDidMount() {
+        document.querySelectorAll('footer a').forEach(anchor => {
+            anchor.target = '_blank';
+            anchor.rel = 'noopener noreferrer';
+        });
+    }
+
+    render() {
+        return (
+            <footer>
+                <div className="footer-primary">
+                    <a className="footer-logo" href="https://www.mozilla.org/">Mozilla</a>
+
+                    <div>
+                        <h3>Mozilla</h3>
+                        <nav>
+                            <ul>
+                                <li><a href="https://www.mozilla.org/about/">About</a></li>
+                                <li><a href="https://www.mozilla.org/contact/">Contact Us</a></li>
+                                <li><a href="https://donate.mozilla.org/">Donate</a></li>
+                                <li>
+                                    <a href="https://wiki.mozilla.org/Webdev/GetInvolved/mozilla.org">
+                                        Contribute to this site
+                                    </a>
+                                </li>
+                                <li className="footer-social">
+                                    <ul>
+                                        <li><a href="https://twitter.com/mozilla">Twitter <span>(@mozilla)</span></a></li>
+                                        <li><a href="https://www.facebook.com/mozilla">Facebook <span>(Mozilla)</span></a></li>
+                                        <li>
+                                            <a href="https://www.instagram.com/mozillagram/">
+                                                Instagram <span>(@mozillagram)</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+
+                    <div>
+                        <h3>Firefox</h3>
+                        <nav>
+                            <ul>
+                                <li><a href="https://www.mozilla.org/firefox/new/">Download Firefox Web browser</a></li>
+                                <li>
+                                    <a href="https://www.mozilla.org/firefox/desktop/">
+                                        Desktop Browser for Mac, Window, Linux
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.mozilla.org/firefox/android/">
+                                        Mobile Browser for Android
+                                    </a>
+                                </li>
+                                <li><a href="https://www.mozilla.org/firefox/ios/">Mobile Browser for iOS</a></li>
+                                <li className="footer-social">
+                                    <ul>
+                                        <li><a href="https://twitter.com/firefox">Twitter <span>(@firefox)</span></a></li>
+                                        <li><a href="https://www.facebook.com/Firefox">Facebook <span>(Firefox)</span></a></li>
+                                        <li><a href="https://www.youtube.com/firefoxchannel">YouTube <span>(firefoxchannel)</span></a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+
+                <div className="footer-secondary">
+                    <ul>
+                        <li><a href="https://www.mozilla.org/privacy/">Privacy</a></li>
+                        <li><a href="https://www.mozilla.org/privacy/websites/#cookies">Cookies</a></li>
+                        <li><a href="https://www.mozilla.org/about/legal/">Legal</a></li>
+                        <li><a href="https://www.mozilla.org/about/legal/fraud-report/">Report Trademark Abuse</a></li>
+                    </ul>
+                    <p>Portions of this content are ©1998–{new Date().getFullYear()} by individual mozilla.org contributors. Content available under a {' '}
+                    <a href="https://www.mozilla.org/foundation/licensing/website-content/">Creative Commons license</a>.</p>
+                </div>
+
+            </footer>
+        );
+    }
+}
