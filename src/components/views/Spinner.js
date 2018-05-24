d0026a2807fde184a71bcefe1d4ed86c112fab02
John Karahalis <john.karahalis@gmail.com>
2018-05-23 22:50:02 -0400

    Switch from react-spinkit to react-spinners
    Unlike react-spinkit, react-spinners has a "size" option. We were able
    to make the react-spinkit spinner bigger with some hacky custom CSS, but
    it made the spinner look weird in most browsers and weirder still in IE.

--- src/components/views/Spinner.js
+++ src/components/views/Spinner.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import Spinner from 'react-spinkit';
+import { PulseLoader } from 'react-spinners';
 
 import './css/Spinner.css';
 
 
 /**
- * Simple wrapper for react-spinkit that uses some default options and applies
+ * Simple wrapper for react-spinners that uses some default options and applies
  * our custom styles.
  */
-export default props => {
-    return <Spinner name="circle" fadeIn="none" {...props} />;
-};
+export default class extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { showSpinner: false };
+    }
+
+    componentDidMount() {
+        // According to Jakob Nielsen's research, "no special feedback is
+        // necessary during delays of more than 0.1 but less than 1.0 second."
+        //
+        // react-spinkit, a competing library, does this by default
+        //
+        // https://www.nngroup.com/articles/response-times-3-important-limits/
+        setTimeout(() => {
+            this.setState({ showSpinner: true });
+        }, 1000);
+    }
+
+    render() {
+        return (
+            <div id="spinner">
+                <PulseLoader
+                    size="20"
+                    loading={this.state.showSpinner}
+                />
+            </div>
+        );
+    }
+}
