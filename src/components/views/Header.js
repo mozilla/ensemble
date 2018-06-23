1eb57180851bfd6f30ae1994c1488f109835fd58
John Karahalis <john.karahalis@gmail.com>
2018-06-22 20:14:16 -0400

    Issue #176: Split menu into Desktop / Mobile
    This commit does not include any mobile support. We decided that it was
    more important to get something sooner than later as Chris will be
    reviewing the site on Tuesday.

--- src/components/views/Header.js
+++ src/components/views/Header.js
@@ -11,11 +11,29 @@ export default () => (
         <h1><Link to="/">Firefox Public Data Report</Link></h1>
         <nav id="main-navigation">
             <ul>
-                {dashboards.map((dashboard, index) => (
-                    <li key={index}>
-                        <Link to={`/dashboard/${dashboard.path}`}>{dashboard.menuTitle}</Link>
-                    </li>
-                ))}
+                {dashboards.sections.map(dashboardSection => {
+                    if (dashboardSection.comingSoon) {
+                        return (
+                            <li key={dashboardSection.key} id={`navigation-${dashboardSection.key}`} className="navigation-coming-soon">
+                                <span className="navigation-section-title">{dashboardSection.title}</span>
+                                <span className="navigation-coming-soon-message">Coming soon</span>
+                            </li>
+                        );
+                    } else {
+                        return (
+                            <li key={dashboardSection.key} id={`navigation-${dashboardSection.key}`} className="navigation-section">
+                                <span className="navigation-section-title">{dashboardSection.title}</span>
+                                <ul className="navigation-section-members">
+                                    {dashboardSection.members.map(dashboard => (
+                                        <li key={dashboardSection.key + dashboard.key}>
+                                            <Link to={`/dashboard/${dashboard.key}`}>{dashboard.menuTitle}</Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </li>
+                        );
+                    }
+                })}
             </ul>
         </nav>
     </header>
