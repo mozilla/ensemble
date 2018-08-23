0d71d8371cf34f31ccb73a312ea2a75489edb549
John Karahalis <john.karahalis@gmail.com>
2018-08-23 16:12:23 -0400

    Address eslint warnings

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,11 +1,4 @@
 import React from 'react';
-import Helmet from 'react-helmet';
-import { Link } from 'react-router-dom';
-
-import StripedHeader from './StripedHeader';
-import { getPageTitle } from '../../lib/utils';
-
-import './css/Home.css';
 
 
 export default () => <p>Coming soon...</p>;
