8bd2a2e59b77c9def0d29b6d6b99ccd5b6c051c8
John Karahalis <john.karahalis@gmail.com>
2018-09-21 15:15:11 -0400

    Issue #196: Generate a version.json file

---
+++ scripts/generateVersionJSON.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const childProcess = require('child_process');
+const fs = require('fs');
+
+const packageJSON = require(path.join(__dirname, '..', 'package.json'));
+
+
+const outFilename = path.join(__dirname, '..', 'version.json');
+
+const versionJSON = {
+    source: 'https://github.com/mozilla/ensemble',
+    version: packageJSON.version,
+    commit: childProcess.execSync('git rev-parse HEAD').toString().trim(),
+};
+
+fs.writeFile(outFilename, JSON.stringify(versionJSON, null, 4), error => {
+    if (error) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+    } else {
+        // eslint-disable-next-line no-console
+        console.log('Wrote ' + outFilename);
+    }
+});
