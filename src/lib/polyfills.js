e4b3a19e7a535f23116a8b7ef1c6cbed255d3133
John Karahalis <john.karahalis@gmail.com>
2019-04-30 13:09:59 -0400

    Remove unused src/lib/polyfills.js file

--- src/lib/polyfills.js
+++
@@ -1,6 +0,0 @@
-// Provide polyfills for use in older browsers.
-//
-// babel-polyfill is big, but importing modules as-needed from core-js is a game
-// of whack-a-mole. We did use core-js in the past, but even after importing
-// many individual modules we were left unsure that we had covered everything.
-import 'babel-polyfill';
